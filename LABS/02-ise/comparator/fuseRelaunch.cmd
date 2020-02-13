03540ec9a0c811e151d2397a8797dd4c2bcc13c2
lab661 <lab661@PC-447.urel.feec.vutbr.cz>
2020-02-13 11:43:45 +0100

    added lab02

---
+++ LABS/02-ise/comparator/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -o "/home/lab661/Documents/xjurek04/Digital-Electronics-1/LABS/02-ise/comparator/top_tb00_isim_beh.exe" -prj "/home/lab661/Documents/xjurek04/Digital-Electronics-1/LABS/02-ise/comparator/top_tb00_beh.prj" "work.top_tb00" 
